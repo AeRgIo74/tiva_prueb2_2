fdd2470beaf5419d2b596c7a17abb30cf8a38474
AeRgIo74 <aergio74@gmail.com>
2024-09-24 12:22:55 -0400

    lab 5 completo

---
+++ examples/boards/ek-tm4c1294xl/timer_prueba_uart_2/gcc/timer_prueba_uart_2.d
@@ -0,0 +1,35 @@
+gcc/timer_prueba_uart_2.o: timer_prueba_uart_2.c \
+ /usr/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h \
+ ../../../../inc/hw_ints.h ../../../../inc/hw_memmap.h \
+ ../../../../inc/hw_types.h ../../../../driverlib/debug.h \
+ ../../../../driverlib/fpu.h ../../../../driverlib/gpio.h \
+ ../../../../driverlib/interrupt.h ../../../../driverlib/pin_map.h \
+ ../../../../driverlib/rom.h ../../../../driverlib/rom_map.h \
+ ../../../../driverlib/sysctl.h ../../../../driverlib/timer.h \
+ ../../../../driverlib/uart.h ../../../../utils/uartstdio.h \
+ /usr/lib/gcc/arm-none-eabi/7.3.1/include/stdarg.h \
+ /usr/arm-none-eabi/include/string.h /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/newlib.h \
+ /usr/arm-none-eabi/include/sys/config.h \
+ /usr/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/arm-none-eabi/include/sys/reent.h \
+ /usr/arm-none-eabi/include/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/7.3.1/include/stddef.h \
+ /usr/arm-none-eabi/include/sys/_types.h \
+ /usr/arm-none-eabi/include/machine/_types.h \
+ /usr/arm-none-eabi/include/sys/lock.h \
+ /usr/arm-none-eabi/include/sys/cdefs.h \
+ /usr/arm-none-eabi/include/sys/string.h \
+ /usr/arm-none-eabi/include/stdio.h \
+ /usr/arm-none-eabi/include/sys/types.h \
+ /usr/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /usr/arm-none-eabi/include/machine/types.h \
+ /usr/arm-none-eabi/include/sys/stdio.h \
+ /usr/arm-none-eabi/include/ctype.h
